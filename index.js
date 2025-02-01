ffe72fcc32a25f618aa7c7b2c30c33583092bcd8
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-02-02 02:27:31 +0600

    Featured Course done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -28,12 +28,44 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const featuredCourseCollection = client.db('courseDB').collection('course')
+        // featureCourse er jonno
+        app.post('/featuredCourse', async (req, res) => {
+            const featureCourse = req.body;
+            console.log(featureCourse);
+            const result = await featuredCourseCollection.insertOne(featureCourse);
+            res.send(result);
+        });
+        app.get('/featuredCourse', async (req, res) => {
+            const cursor = featuredCourseCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+        app.get('/featuredCourse/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await featuredCourseCollection.findOne(query);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
